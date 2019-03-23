7cdc46bf12287223c0ea245dc71b1f0d5bd645c4
Med ben Malek <mohamed.benmalekcherif@esprit.tn>
2019-03-23 01:53:28 +0100

    up

---
+++ src/UserBundle/Form/UserType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('bornAt')
+            ->add('email')
+            ->add('phone')
+            ->add('address')
+            ->add('zipCode')
+            ->add('city')
+            ->add('description')
+            ->add('state');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'userbundle_user';
+    }
+
+
+}
---
+++ src/UserBundle/Form/UserTypeNew.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserTypeNew extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('username')
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'options' => array(
+                    'translation_domain' => 'FOSUserBundle',
+                    'attr' => array(
+                        'autocomplete' => 'new-password',
+                    ),
+                ),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch'
+            ))
+            ->add('bornAt')
+            ->add('email')
+            ->add('phone')
+            ->add('address')
+            ->add('zipCode')
+            ->add('city')
+            ->add('description')
+            ->add('state');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'userbundle_user';
+    }
+
+
+}
